f65b428a4577cb81f1ca8b72f810b98a046cf323
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-01-20 22:55:56 +0300

    feat: task 03-files-in-folder complete (Sat, Jan 20, 2024 10:55:56 PM)

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function outputInformation() {
+  try {
+    const folderPath = path.join(__dirname, 'secret-folder');
+    const folderContent = await fs.readdir(folderPath, {
+      withFileTypes: true,
+    });
+    for (const item of folderContent) {
+      const filePath = path.join(folderPath, item.name);
+      const fileStats = await fs.stat(filePath);
+      const fileName = path.parse(item.name).name;
+      const fileExtension = path.extname(item.name).slice(1);
+      const fileSize = fileStats.size / 1024;
+      console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+    }
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+}
+
+outputInformation();
