07a00bd71d998ab8db7e0474175715524a2ba93f
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-01-23 14:10:46 +0300

    fix: fix 03-files-in-folder task (Tue, Jan 23, 2024 02:10:46 PM)

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,11 +9,15 @@ async function outputInformation() {
     });
     for (const item of folderContent) {
       const filePath = path.join(folderPath, item.name);
-      const fileStats = await fs.stat(filePath);
-      const fileName = path.parse(item.name).name;
-      const fileExtension = path.extname(item.name).slice(1);
-      const fileSize = fileStats.size / 1024;
-      console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+      if (item.isFile()) {
+        const fileStats = await fs.stat(filePath);
+        const fileName = path.parse(item.name).name;
+        const fileExtension = path.extname(item.name).slice(1);
+        const fileSize = fileStats.size / 1024;
+        console.log(
+          `${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`,
+        );
+      }
     }
   } catch (error) {
     console.error(`Error: ${error.message}`);
