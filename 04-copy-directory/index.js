3b1cd5638166e8ddbe8b413a1abb57b98396ec42
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-01-23 14:54:19 +0300

    fix: fix 04-copy-directory task (Tue, Jan 23, 2024 02:54:19 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,14 +6,30 @@ async function copyDir() {
   const filesCopyFolder = path.join(__dirname, 'files-copy');
   try {
     await fs.mkdir(filesCopyFolder, { recursive: true });
-    const files = await fs.readdir(filesFolder);
-    for (const file of files) {
-      const fileFrom = path.join(filesFolder, file);
-      const fileTo = path.join(filesCopyFolder, file);
-      await fs.copyFile(fileFrom, fileTo);
-    }
+    await copyFiles(filesFolder, filesCopyFolder);
+    fs.watch(filesFolder, { recursive: true }, async (eventType, filename) => {
+      if (eventType === 'change' || eventType === 'rename') {
+        await copyFiles();
+      } else if (eventType === 'unlink') {
+        const fileToDelete = path.join(filesCopyFolder, filename);
+        try {
+          await fs.unlink(fileToDelete);
+        } catch (error) {
+          console.log(`Error: ${error}`);
+        }
+      }
+    });
   } catch (error) {
     console.log(`Error: ${error}`);
+  }
+}
+
+async function copyFiles(from, to) {
+  const files = await fs.readdir(from);
+  for (const file of files) {
+    const fileFrom = path.join(from, file);
+    const fileTo = path.join(to, file);
+    await fs.copyFile(fileFrom, fileTo);
   }
 }
 
