37dc92b5912249cdaca06f028f1b86498c585b85
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2024-01-23 17:59:51 +0300

    fix: fix#2 04-copy-directory task (Tue, Jan 23, 2024 05:59:51 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,35 +1,31 @@
-const fs = require('fs/promises');
+const fs = require('fs').promises;
 const path = require('path');
 
 async function copyDir() {
-  const filesFolder = path.join(__dirname, 'files');
-  const filesCopyFolder = path.join(__dirname, 'files-copy');
-  try {
-    await fs.mkdir(filesCopyFolder, { recursive: true });
-    await copyFiles(filesFolder, filesCopyFolder);
-    fs.watch(filesFolder, { recursive: true }, async (eventType, filename) => {
-      if (eventType === 'change' || eventType === 'rename') {
-        await copyFiles();
-      } else if (eventType === 'unlink') {
-        const fileToDelete = path.join(filesCopyFolder, filename);
-        try {
-          await fs.unlink(fileToDelete);
-        } catch (error) {
-          console.log(`Error: ${error}`);
-        }
-      }
-    });
-  } catch (error) {
-    console.log(`Error: ${error}`);
-  }
-}
+  const sourceDir = path.join(__dirname, 'files');
+  const destDir = path.join(__dirname, 'files-copy');
 
-async function copyFiles(from, to) {
-  const files = await fs.readdir(from);
-  for (const file of files) {
-    const fileFrom = path.join(from, file);
-    const fileTo = path.join(to, file);
-    await fs.copyFile(fileFrom, fileTo);
+  try {
+    await fs.mkdir(destDir, { recursive: true });
+
+    const sourceFiles = await fs.readdir(sourceDir);
+    const destFiles = await fs.readdir(destDir);
+
+    for (const file of destFiles) {
+      if (!sourceFiles.includes(file)) {
+        const filePath = path.join(destDir, file);
+        await fs.unlink(filePath);
+      }
+    }
+
+    for (const file of sourceFiles) {
+      const sourceFile = path.join(sourceDir, file);
+      const destFile = path.join(destDir, file);
+
+      await fs.copyFile(sourceFile, destFile);
+    }
+  } catch (error) {
+    console.error(`${error}`);
   }
 }
 
